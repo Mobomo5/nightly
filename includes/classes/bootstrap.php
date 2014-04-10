8c86d85ba0cccc43667e5a1017cc70ce3b7b7be0
EHQhelp <info@ehqhelp.net>
2014-04-09 17:27:10 -0700

    Index

---
+++ includes/classes/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Keegan Laur
+ * Date: 4/9/14
+ * Time: 5:20 PM
+ */
+
+class bootstrap {
+    public function __construct() {
+
+    }
+}
--- index.php
+++ index.php
@@ -4,4 +4,13 @@
  * User: Keegan Laur
  * Date: 4/9/14
  * Time: 12:52 PM
- */ 
+ */
+session_start();
+header('X-Powered-By: Educask 3.0');
+header('X-Frame-Options: DENY');
+define('EDUCASK_ROOT', getcwd());
+define('EDUCASK_WEB_ROOT', dirname($_SERVER['SCRIPT_NAME']) . '/');
+
+//Start EHQ Simple CMS
+require_once(EDUCASK_ROOT . '/includes/classes/bootstrap.php');
+new bootstrap()->run();
