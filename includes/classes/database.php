1baea48928668f4097543f1440742ac1eb30b6ae
Craig Isenor <cisenor1@gmail.com>
2014-04-10 17:29:42 -0700

    Working on database class

---
+++ includes/classes/database.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Craig
+ * Date: 4/10/14
+ * Time: 5:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class database {
+    
+}
