0c7c56d0e987882392f377b6fe82ce6d551f3eb9
EHQhelp <info@ehqhelp.net>
2014-05-21 18:37:52 -0700

    Theme changes. More organized.

--- includes/classes/bootstrap.php
+++ includes/classes/bootstrap.php
@@ -153,9 +153,7 @@ class bootstrap {
         }
         $loader->addPath(EDUCASK_ROOT . '/includes/baseThemes/html5');
         $loader->addPath(EDUCASK_ROOT . '/includes/baseThemes');
-        $twig = new Twig_Environment($loader, array(
-            'debug' => true,
-        ));
+        $twig = new Twig_Environment($loader, array('debug' => true,));
         $twig->addExtension(new Twig_Extension_Debug());
         echo $twig->render('index.twig', array('site' => $this->site, 'blocks' => $this->blocks));
     }
