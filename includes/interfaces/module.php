463191f3e707e324ab99bd05c4b8f350123e004d
EHQhelp <info@ehqhelp.net>
2014-05-29 19:44:21 -0700

    New needed function in modules.

--- includes/interfaces/module.php
+++ includes/interfaces/module.php
@@ -3,6 +3,7 @@
 interface module {
     public function __construct();
     public static function getPageType();
+    public function getPageContent();
     public function noGUI();
     public function getReturnPage();
 }
