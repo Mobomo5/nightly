9149561ec83df9796e6e3976e961af081d95e486
EHQhelp <info@ehqhelp.net>
2014-06-04 21:46:33 -0700

    Fixes.

--- includes/classes/nodeEngine.php
+++ includes/classes/nodeEngine.php
@@ -6,7 +6,12 @@
  * Time: 11:05 AM
  */
 require_once(DATABASE_OBJECT_FILE);
-
+require_once(NODE_ENGINE_OBJECT_FILE);
+require_once(NODE_OBJECT_FILE);
+require_once(NODE_TYPE_OBJECT_FILE);
+require_once(NODE_FIELD_REVISION_OBJECT_FILE);
+require_once(NODE_FIELD_TYPE_OBJECT_FILE);
+require_once(NODE_FIELD_OBJECT_FILE);
 class nodeEngine {
     private static $instance;
 
@@ -19,5 +24,21 @@ class nodeEngine {
 
     private function __construct() {
         //Do nothing.
+    }
+    public function getNode($nodeID) {
+        if(! is_numeric($nodeID)) {
+            return false;
+        }
+        $database = database::getInstance();
+        if(! $database->isConnected()) {
+            return false;
+        }
+        $results = $database->getData('title, nodeType', 'node', "nodeID={$nodeID}");
+        if($results == false) {
+            return false;
+        }
+        if($results == null) {
+            return false;
+        }
     }
 }
