1d7e86a0ce9a04b4f0c6a825f8c30abe28571f1a
EHQhelp <info@ehqhelp.net>
2014-06-16 10:30:38 -0700

    Safety fix.

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ error_reporting(E_ALL);
 header('X-Powered-By: Educask 3.0');
 header('X-Generator: Educask 3.0 (http://www.educask.com)');
 header('X-Frame-Options: DENY');
+header('X-XSS-Protection: 1; mode=block');
 
 ini_set('session.use_only_cookies', true);
 ini_set('session.cookie_httponly', true);
